aa480a25473785dc965258d3e537911d8bbe19c1
viralgupta <guptaviral0@gmail.com>
2024-06-22 06:07:51 +0530

    fix: binarypath is properly resolved now

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const { spawn } = require("child_process");
 const fs = require("fs");
 const path = require("path");
-const {getBinaryName, normalize} = require("./utils.js");
+const {getBinaryName, normalize, getAuthInfo} = require("./utils.js");
+const constants = require("./constants.js");
 
 class NeutralinoApp {
+
   url = "";
   windowOptions = {};
-  ws = null;
-  retryHandler = null;
   authInfo = null;
+  reconnecting = false;
+  retryHandler = null;
+  ws = null;
   nativeCalls = {};
   offlineMessageQueue = [];
   neuProcess = null;
@@ -18,18 +21,13 @@ class NeutralinoApp {
     this.windowOptions = windowOptions;
   }
 
-  _retryLater() {
-    this.reconnecting = true;
-    this.retryHandler = setTimeout(() => {
-      this.reconnecting = false;
-      this._startWebsocket();
-    }, 1000);
-  }
-
   init() {
+
+    // this._startWebsocket()
+
     const EXEC_PERMISSION = 0o755;
 
-    let outputArgs = " --url=" + normalize(this.url);
+    let outputArgs = " --path=" + normalize(this.url);
 
     for (let key in this.windowOptions) {
       if (key == "processArgs") continue;
@@ -51,9 +49,9 @@ class NeutralinoApp {
       return console.error(`Unsupported platform or CPU architecture: ${process.platform}_${arch}`);
     }
 
-    let binaryPath = `bin${path.sep}${binaryName}`;
+    let binaryPath = path.join(this.url,`bin${path.sep}${binaryName}`);
 
-    let args = " --load-dir-res --path=. --export-auth-info --neu-dev-extension";
+    let args = " --load-dir-res --export-auth-info --neu-dev-extension";
 
     if (outputArgs) args += " " + outputArgs;
 
@@ -64,16 +62,93 @@ class NeutralinoApp {
 
     this.neuProcess = spawn(binaryPath, args.split(` `), { stdio: "inherit" });
 
-    this.neuProcess.on("exit", function (code) {
+    this.neuProcess.on("exit", (code) => {
       let statusCodeMsg = code ? `error code ${code}` : `success code 0`;
       let runnerMsg = `${binaryName} was stopped with ${statusCodeMsg}`;
       console.warn(runnerMsg);
+
+      this._stopWebsocket()
 
       if (this.windowOptions && this.windowOptions.exitProcessOnClose) {
         process.exit(code);
       }
     });
   }
+
+  _retryLater() {
+    this.reconnecting = true;
+    this.retryHandler = setTimeout(() => {
+      this.reconnecting = false;
+      this._startWebsocket();
+    }, 1000);
+  }
+
+  _startWebsocket = () => {
+    this.authInfo = getAuthInfo(this.url);
+
+    console.log("Auth info: ", this.authInfo);
+  
+    if (!this.authInfo) {
+      this._retryLater();
+      return;
+    }
+
+    this.ws = new WS(`ws://127.0.0.1:${this.authInfo.nlPort}?extensionId=js.neutralino.devtools&connectToken=${this.authInfo.nlConnectToken}`);
+  
+    this.ws.onerror = () => {
+      this._retryLater();
+      return;
+    };
+  
+    this.ws.onopen = () => {
+      console.log("Connected with the application.");
+    };
+  
+    this.ws.onclose = () => {
+      console.log("Connection closed.");
+    };
+  
+    this.ws.onmessage = (e) => {
+      if (typeof e.data === "string") {
+        const message = JSON.parse(e.data);
+        console.log("Received message: ", message);
+  
+        if (message.id && message.id in this.nativeCalls) {
+          // Native call response
+          if (message.data?.error) {
+            this.nativeCalls[message.id].reject(message.data.error);
+            if (message.data.error.code == 'NE_RT_INVTOKN') {
+              // Invalid native method token
+              // handleNativeMethodTokenError();
+            }
+          }
+          else if (message.data?.success) {
+            this.nativeCalls[message.id].resolve(message.data.hasOwnProperty('returnValue') ? message.data.returnValue : message.data);
+          }
+          delete this.nativeCalls[message.id];
+        }
+        else if(message.event) {
+          // Event from process
+          if(message.event == 'openedFile' && message?.data?.action == 'dataBinary') {
+              message.data.data = base64ToBytesArray(message.data.data);
+          }
+          this.emit(message.event, message.data);
+      }
+      }
+    }
+  };
+  
+  _stopWebsocket = () => {
+    if (this.retryHandler) {
+      clearTimeout(this.retryHandler);
+    }
+    if (this.ws) {
+      this.ws.close();
+      if (fs.existsSync(constants.files.authFile)) {
+        fs.unlinkSync(constants.files.authFile);
+      }
+    }
+  };
 
   close() {
     if (this.neuProcess) {
